9b3bee181181ae674e5e7556ea61b8a54ad3729b
Giovanni Incammicia <giovanni.incammicia@gmail.com>
2023-11-17 17:16:56 +0100

    Add Jest fetch mock and update auth and chat functions

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,13 +1,78 @@
-import uuid from '../index';
+import { auth } from '../index';
+import fetchMock from 'jest-fetch-mock';
 
-describe('valid UUID', () => {
-  let VALID_UUID_REGEX: RegExp;
+describe('auth', () => {
+  it('should return a token when given a valid apiKey', async () => {
+    const apiKey = 'valid-api-key';
+    const token = 'valid-token';
+    jest.spyOn(global, 'fetch').mockResolvedValueOnce({
+      json: () => Promise.resolve({ token }),
+    } as any);
 
-  beforeAll(() => {
-    VALID_UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+    const result = await auth({ apiKey });
+
+    expect(result).toEqual(token);
+    expect(global.fetch).toHaveBeenCalledWith(`/auth/${apiKey}`);
   });
 
-  test('should match a valid UUID', () => {
-    expect(VALID_UUID_REGEX.test(uuid.v4())).toBeTruthy();
+  it('should throw an error when no data is returned from the API', async () => {
+    const apiKey = 'invalid-api-key';
+    jest.spyOn(global, 'fetch').mockResolvedValueOnce({
+      json: () => Promise.resolve(null),
+    } as any);
+
+    await expect(auth({ apiKey })).rejects.toThrow('No data returned from auth');
+    expect(global.fetch).toHaveBeenCalledWith(`/auth/${apiKey}`);
+  });
+
+  it('should throw an error when an error is returned from the API', async () => {
+    const apiKey = 'invalid-api-key';
+    const error = 'Invalid API key';
+    jest.spyOn(global, 'fetch').mockResolvedValueOnce({
+      json: () => Promise.resolve({ error }),
+    } as any);
+
+    await expect(auth({ apiKey })).rejects.toThrow(error);
+    expect(global.fetch).toHaveBeenCalledWith(`/auth/${apiKey}`);
+  });
+
+  it('should throw an error when no token is returned from the API', async () => {
+    const apiKey = 'invalid-api-key';
+    jest.spyOn(global, 'fetch').mockResolvedValueOnce({
+      json: () => Promise.resolve({}),
+    } as any);
+
+    await expect(auth({ apiKey })).rejects.toThrow('No token returned from auth');
+    expect(global.fetch).toHaveBeenCalledWith(`/auth/${apiKey}`);
+  });
+});
+import { chat } from '../index';
+
+describe('chat', () => {
+  beforeEach(() => {
+    fetchMock.resetMocks();
+  });
+
+  it('should call the chat endpoint with the correct arguments', async () => {
+    const body = { message: 'Hello, world!' };
+    fetchMock.mockResponseOnce(JSON.stringify({}));
+
+    await chat(body);
+
+    expect(fetchMock.mock.calls.length).toEqual(1);
+    expect(fetchMock.mock.calls[0][0]).toEqual('/chat');
+    expect(fetchMock.mock.calls[0][1]).toMatchObject({
+      method: 'POST',
+      body: JSON.stringify(body),
+    });
+  });
+
+  it('should return the response body', async () => {
+    const responseBody = { message: 'Hello, world!' };
+    fetchMock.mockResponseOnce(JSON.stringify(responseBody));
+
+    const result = await chat({});
+
+    expect(result).toEqual(responseBody);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -6,59 +6,91 @@
 import fetchApi from './helpers/fetch';
 
 // QUESTIONS:
-// questa sdk verrà usata da applicativi frontend? In tal caso, come abilitiamo cors su cinzia-server?
-// cinzia-server in caso di errore restituisce "token: 'none'", forse sarebbe meglio ometterlo per avere un tipo di errore più uniforme con le altre chiamate
-// il token è utilizzabile una sola volta, ma non ha scadenza?
-// chat vuole un chatId, ma come fa il client a recuperarlo?
-// chat vuole uno userId, ma come fa il client a recuperarlo?
-// mode non dovrebbe essere un'enum? I valori sono solo "turbo" e "title"?
+// questa sdk verrà usata da applicativi frontend? In tal caso, come abilitiamo cors su cinzia-server? CORS ABILITATO *
+// il token è utilizzabile una sola volta, ma non ha scadenza? METTERE EXPIREAT
+// chat vuole un chatId, ma come fa il client a recuperarlo? FARE NUOVO ENDPOINT IN CINZIA-SERVER CHE NON PRENDA CHATID, MA UNA CHAT INTERA
+// Del modello Chat su cinzia-server in Prisma, prendi messages (solo time, role, content) e modelId (se non lo passi, usi cinzia normale)
+// poi usa standardModel o dynamicContext in base al valore di modelId
+
+// chat vuole uno userId, ma come fa il client a recuperarlo? (LO PRENDI IN CINZIA-SERVER DURANTE AUTH NELLA CHIAMATA CHAT)
 
 type ApiAuthResponse = {
   token: string;
+  userId: string;
 };
 type ApiErrorResponse = {
   error: string;
 };
 
 /**
- * Authenticates the user with the given API key and returns a token.
+ * Authenticates the user with the given API key and returns a token. It should be used only server-side.
  * @param options - The options object.
  * @param options.apiKey - The API key to use for authentication.
- * @returns A Promise that resolves with the authentication token.
+ * @returns A Promise that resolves with the an object containing the authentication token and the user id.
  * @throws If there is an error with the API request or if the response is invalid.
  */
 export async function auth({ apiKey }: { apiKey: string }) {
-  const response = await fetchApi(`/auth/${apiKey}`);
+  if (window) console.warn('ONLY USE SERVER-SIDE');
+  const response = await fetchApi('/auth', { method: 'POST', body: JSON.stringify({ apiKey }) });
   const data = (await response.json()) as ApiAuthResponse | ApiErrorResponse;
   if (!data) throw new Error('No data returned from auth');
   if ('error' in data) throw new Error(data.error ?? 'Unknown error');
-  if (!data.token) throw new Error('No token returned from auth');
-  return data.token;
+  const { token, userId } = data;
+  if (!token) throw new Error('No token returned from auth');
+  return { token, userId };
 }
+
+type Message = {
+  time: string;
+  role: 'user' | 'assistant' | 'system' | 'dev';
+  token: number;
+  content: string;
+  source: string;
+};
 
 type ApiChatRequest = {
   locale: string;
-  chatId: string;
-  mode: 'turbo' | 'title';
-  userId: string;
+  messages: Message[]; // TODO: fare tipo
+  mode: 'turbo' | 'title' | 'standard';
   token: string;
+  modelId?: string;
 };
 
 /**
  * Sends a chat request to cinzia-server.
  * @param body - The request body.
  * @param body.locale - The locale to use for the chat.
- * @param body.chatId - The ID of the chat.
- * @param body.mode - The chat mode. Can be "turbo" or "title".
- * @param body.userId - The ID of the user.
+ * @param body.messages - TODO
+ * @param body.mode - The chat mode. Can be "turbo", "title" or "standard".
  * @param body.token - The authentication token to be taken with the "auth" function.
+ * @param body.modelId - The ID of the Model created on Cinzia.app.
+ * @param callback - A function that's called every time a new message is received from the server.
  * @returns A Promise that resolves with the response body as a text/event-stream.
  */
-export async function chat(body: ApiChatRequest) {
+export async function chat(body: ApiChatRequest, callback: (data: string) => void) {
   const response = await fetchApi('/chat', {
     method: 'POST',
     body: JSON.stringify(body),
   });
+  if (!response || !response.body) throw new Error('No response from chat');
 
-  return response.body;
+  const reader = response.body.getReader();
+  const decoder = new TextDecoder();
+  const stream = { reader, decoder };
+
+  let done = false;
+  let text = '';
+
+  while (!done) {
+    const { value } = await stream.reader.read();
+    if (!value) {
+      done = true;
+      break;
+    }
+
+    const chunkValue = stream.decoder.decode(value);
+    text += chunkValue;
+    callback(text);
+  }
+  return text;
 }
