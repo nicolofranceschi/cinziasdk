320b7ac6a987170185d66af60786488f572c9b59
Giovanni Incammicia <giovanni.incammicia@gmail.com>
2024-01-05 14:15:32 +0000

    Add skipTest function to index.test.ts

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -2,9 +2,19 @@ const API_KEY = '6585661277b7998ecb53c1c1';
 import { describe, it, expect } from '@jest/globals';
 import { auth, chat } from '../index';
 
+function skipTest () {
+  if (!process.env.CINZIA_SERVER_URL) {
+    console.warn('Missing CINZIA_SERVER_URL env variable, skipping test');
+    expect(true).toBe(true);
+    return true;
+  }
+  return false;
+}
+
 describe('index', () => {
   let token: string;
   it('should reply', async () => {
+    if (skipTest()) return;
     const user = await auth({ apiKey: API_KEY });
     token = user.token;
     const res = await chat({
@@ -16,6 +26,7 @@ describe('index', () => {
     expect(typeof res).toBe('string');
   });
   it('should reply with', async () => {
+    if (skipTest()) return;
     const res = await chat({
       locale: 'it',
       messages: [
